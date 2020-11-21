c7d98f8efb8e07a8c561db4fdc8a6929aaf62a41
Mii1122 <seventeen3310.17mi@gmail.com>
2020-11-21 21:25:47 +0900

    2-11課題提出

--- 2-11/result.php
+++ 2-11/result.php
@@ -4,5 +4,5 @@ $mail = $_POST['mail'];
 $password = $_POST['password'];
 ?>
 <p>私の名前は、<?php echo $my_name; ?></p>
-<p>メールアドレスは、<?php echo $_mail; ?></p>
+<p>メールアドレスは、<?php echo $mail; ?></p>
 <p>私のパスワードは、<?php echo $password; ?></p>

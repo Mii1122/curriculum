846fcb026e034094d8d3dcbb9181067b4e94c2c9
Mii1122 <seventeen3310.17mi@gmail.com>
2020-11-21 21:22:38 +0900

    2-11課題提出

---
+++ 2-11/index.php
@@ -0,0 +1,16 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="text" name="mail" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+<?php
+echo "<br />";
+//1,仕様書・設計書 物事の順序を記載したもの
+//2,Git 変更履歴を記録、追跡するための管理システム
+//3,マージツール 決められたルールに従って統合すること
+?>
